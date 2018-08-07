bc1b9f9d115d233e073b01d8ea74301b81259b61
Yolanda Fay <yolandafay@gmail.com>
2018-08-07 02:14:10 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -14,13 +14,12 @@
 
 def speak_to_grandma(say_what)
   
-  while say_what != say_what.upcase
-  
   if say_what == say_what.upcase 
-    puts "NO, NOT SINCE 1938!"
+    return "NO, NOT SINCE 1938!"
   elsif say_what == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!"
   else 
-    puts "HUH?! SPEAK UP, SONNY!"
+    return "HUH?! SPEAK UP, SONNY!"
   end
+  
 end 
